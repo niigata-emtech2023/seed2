ac35c126ca575416d133825bf7743da088f1c7d0
emtech-user <emtech-user@192.168.128.111>
2023-05-31 11:00:34 +0900

    阿部　てｓｔ

--- spofes2/src/main/java/model/dao/ContactDAO.java
+++ spofes2/src/main/java/model/dao/ContactDAO.java
@@ -5,57 +5,36 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import model.entity.SpoFesBean;
-
 
 
 public class ContactDAO {
-	public int checkTask(SpoFesBean spofes) throws SQLException, ClassNotFoundException {
+	public int checkTask(String point,String teamname) throws SQLException, ClassNotFoundException {
 
 		int count = 0; //処理件数
+		
+		String selectsql="SELECT point,team_id FROM t_rank r,m_team t WHERE t.team_id = r.team_id AND team_name = ?";
+		String insertsql="UPDATE m_team SET point = ? WHERE team_id = ?";
 
 		// データベースへの接続の取得、PreparedStatementの取得
 		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pstmt = con.prepareStatement("INSERT INTO m_point VALUE(?, ?, sysdate, ?, ?)")) {
-
-			// DTOからのデータの取り出し
-			int teamId = spofes.getTeamId();
-			int taskId = spofes.getTaskId();
-			int pointAdd = spofes.getPointAdd();
-			int pointSub = spofes.getPointSub();
-
+				PreparedStatement pstmt = con.prepareStatement(selectsql)) {
+			
+			
 			// プレースホルダへの値の設定
-			pstmt.setInt(1, teamId);
-			pstmt.setInt(2, taskId);
-			pstmt.setInt(3, pointAdd);
-			pstmt.setInt(4, pointSub);
+			pstmt.setString(1, teamname);
+			ResultSet res = pstmt.executeQuery();
+			point += res.getInt("point");
+			
+			PreparedStatement pstmt2 = con.prepareStatement(insertsql);
+			pstmt2.setString(1, point);
+			pstmt2.setInt(2, res.getInt("team_id"));
+			
 
 			// SQLステートメントの実行
-			count = pstmt.executeUpdate();
+			count = pstmt2.executeUpdate();
 		}
 
 		return count;
 
-	}
-
-	public int searchTeamId(String team_name) throws SQLException, ClassNotFoundException {
-
-		int team_id=-1;
-
-		String sql = "SELECT * FROM m_team WHERE team_name = ?";
-
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pstmt = con.prepareStatement(sql)) {
-
-			pstmt.setString(1, team_name);
-			ResultSet res = pstmt.executeQuery();
-
-			team_id = res.getInt("team_id");
-
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-
-	return team_id;	
 	}
 }
